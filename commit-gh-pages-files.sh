46b2cca124004da766e293219714e2f9a2f813df
[skoelzer] <stevenkoelzer@uni-koblenz.de>
2018-10-04 14:55:25 +0200

    deploy site

---
+++ commit-gh-pages-files.sh
@@ -0,0 +1,6 @@
+
+
+
+hugo
+cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
+
---
+++ content/posts/my-first-post.md
@@ -0,0 +1,6 @@
+---
+title: "My First Post"
+date: 2018-10-04T14:24:15+02:00
+draft: true
+---
+
